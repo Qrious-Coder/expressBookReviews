71dbd0040f52bfb80cb8a35005361dba0137a9ba
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-07-02 05:34:07 +0800

    Update general.js
    Add async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,41 +18,107 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: 'Unable to register user.'})
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  res.send(JSON.stringify(books,null,4))
+// Get the book list with async/await
+public_users.get('/', async function (req, res) {
+    // Simulate 2' delay with Promise
+    const getBooks = () => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(books);
+            }, 2000);  
+        });
+    };
+
+    try {
+        const booksData = await getBooks();
+        res.send(JSON.stringify(booksData,null,4));
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'An error occurred!'});
+    }
 });
+
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn
-  return res.status(200).json(books[isbn]);
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let filteredByAuthor = []
-  Object.keys(books).forEach((key) => {
-      if( books[key].author === author ){
-        filteredByAuthor.push(books[key])
-      }
-  })
-  return res.status(200).json(filteredByAuthor);
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    const getBookByIsbn = (isbn) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const book = books[isbn]
+                if (book) {
+                    resolve(book)
+                } else {
+                    reject(new Error('Book unfound'))
+                }
+            }, 2000);
+        });
+    };
+
+    try {
+        const bookData = await getBookByIsbn(isbn)
+        res.status(200).json(bookData);
+    } catch (error) {
+        console.error(error)
+        res.status(404).json({message: 'Book unfound'})
+    }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  let filteredByTitle = []
-  Object.keys(books).forEach((key) => {
-    if( books[key].title === title ){
-        filteredByTitle.push(books[key]) 
+// Get book details based on author
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author
+
+    const getBooksByAuthor = (author) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let filteredByAuthor = []
+                Object.keys(books).forEach((key) => {
+                    if (books[key].author === author) {
+                        filteredByAuthor.push(books[key])
+                    }
+                });
+                resolve(filteredByAuthor)
+            }, 2000); 
+        });
+    };
+
+    try {
+        const booksData = await getBooksByAuthor(author)
+        res.status(200).json(booksData)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'Server error!'})
     }
-  })
-  return res.status(200).json(filteredByTitle);
 });
+
+
+// Get book based on title
+public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title
+
+    const getBooksByTitle = (title) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let filteredByTitle = []
+                Object.keys(books).forEach((key) => {
+                    if (books[key].title === title) {
+                        filteredByTitle.push(books[key]) 
+                    }
+                });
+                resolve(filteredByTitle);
+            }, 2000)
+        });
+    };
+
+    try {
+        const booksData = await getBooksByTitle(title)
+        res.status(200).json(booksData)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'An error occurred'})
+    }
+});
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
